912cad8bd0721cd95b7d87c4794e87df6c00e7ce
Akrammallah <153257896+Akrammallah@users.noreply.github.com>
2024-03-29 12:30:31 +0500

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// 4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the
+// following, including the quotation marks:
+let Famous_person = "Albert Einstein ";
+let message = "“A person who never made a mistake never tried anything new.”";
+console.log(`${Famous_person},once said ${message}`);
